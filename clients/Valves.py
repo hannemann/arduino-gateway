db565bdc99904e49be011c22b7a19e3fd2646fed
hannemann <hannemann666@gmx.net>
2017-04-17 15:39:58 +0200

    add set valve

--- clients/Valves.py
+++ clients/Valves.py
@@ -1,5 +1,7 @@
 from clients.Db import Db, sqlite3
-from config import config
+from config import configparser, config
+import sys
+import subprocess
 
 
 class Valves(Db):
@@ -11,20 +13,30 @@ class Valves(Db):
         'group by addr order by addr;'
     payload = None
     receiver_id = None
+    controller = None
 
     def __init__(self, gateway):
         self.gateway = gateway
         Db.__init__(self)
+        self.get_controller()
 
     def execute(self, payload):
         self.payload = payload
-        if self.parse():
+        if "GET" == self.parse():
             self.gateway.write(self.fetch())
+        if "SET" == self.parse():
+            group = self.get_valve_control_group_name_by_addr(str(self.payload[2]))
+            temp = str(self.payload[3]).strip(chr(0))
+            if isinstance(group, basestring) and isinstance(temp, basestring):
+                subprocess.call([self.controller, group, temp])
 
     def parse(self):
         if len(self.payload) > 3 and self.payload[1].find('G/V') == 0:
             self.receiver_id = self.payload[0]
-            return True
+            return "GET"
+        if len(self.payload) > 3 and self.payload[1].find('G/S') == 0:
+            self.receiver_id = self.payload[0]
+            return "SET"
         return False
 
     def fetch(self):
@@ -40,22 +52,53 @@ class Valves(Db):
             self.receiver_id,
             "V"
         ]
+        names = []
+        name = ''
         for row in self.cursor:
             if row is not None:
-                res = [
-                    str(row["addr"]),
-                    str(self.get_valve_name_by_addr(str(row["addr"]))),
-                    str(row["wanted"] / float(100)),
-                    str(row["real"] / float(100))
-                ]
-                values.append('/'.join(res))
+                name = str(self.get_valve_group_name_by_addr(str(row["addr"])))
+                if name not in names:
+                    names.append(name)
+                    res = [
+                        str(row["addr"]),
+                        name,
+                        str(row["wanted"] / float(100)),
+                        str(row["real"] / float(100))
+                    ]
+                    values.append('/'.join(res))
 
         return "|".join(values)
+
+    def get_controller(self):
+        try:
+            self.controller = config.get(self.config_section, "controller")
+        except configparser.NoSectionError:
+            print('Section "' + self.config_section + '" not found in configuration')
+            sys.exit(1)
+        except configparser.NoOptionError:
+            print('Option "file" not found in configuration')
+            sys.exit(1)
 
     @staticmethod
     def get_valve_name_by_addr(addr):
         for item in config.items('valves'):
             if str(item[1]) == str(addr):
+                return item[0]
+
+        return False
+
+    @staticmethod
+    def get_valve_group_name_by_addr(addr):
+        for item in config.items('valve_groups'):
+            if str(addr) in str(item[1]).split(','):
+                return item[0]
+
+        return False
+
+    @staticmethod
+    def get_valve_control_group_name_by_addr(addr):
+        for item in config.items('valve_control_groups'):
+            if str(addr) in str(item[1]).split(','):
                 return item[0]
 
         return False
