d9cdc1627bccfe4f284ee08780dafa103b1d0cb8
hannemann <hannemann666@gmx.net>
2017-04-18 22:11:16 +0200

    add toggle debug signal handler

--- gateway.py
+++ gateway.py
@@ -2,10 +2,12 @@
 
 import sys
 import serial.tools.list_ports
+import serial.serialutil
 from clients.Temperature import Temperature
 from clients.Valves import Valves
 from clients.Pilight import Pilight
 from config import config
+import signal
 
 baud_rate = 115200
 
@@ -38,14 +40,19 @@ class Gateway:
         for client in clients:
             self.clients.append(client(self))
 
+        signal.signal(signal.SIGUSR1, self.sig_handler)
+
     def run(self):
         try:
             while True:
-                message = self.ser.readline()
-                print(message)
-                payload = message.split()
-                for client in self.clients:
-                    client.execute(payload)
+                try:
+                    message = self.ser.readline()
+                    print(message)
+                    payload = message.split()
+                    for client in self.clients:
+                        client.execute(payload)
+                except serial.serialutil.SerialException:
+                    pass
 
         except KeyboardInterrupt:
             for client in self.clients:
@@ -55,6 +62,10 @@ class Gateway:
         self.ser.write(message.encode('utf-8'))
         self.ser.write('\n'.encode('utf-8'))
         self.ser.flush()
+
+    def sig_handler(self, signum, frame):
+        if signum == signal.SIGUSR1:
+            self.write("1|TD|NULL")
 
 
 def main(argv):
